bd852c7034aff3ec89fddc0f1d8d19b2667ab746
Souhaila <souhaila.elmokhtari@outlook.com>
2023-11-11 16:26:17 +0100

    commit

--- Angular_front-end/src/main.ts
+++ Angular_front-end/src/main.ts
@@ -1,27 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent form submission
-   
-  var password = document.getElementById('password').value;
-   
-  // Check password criteria using regular expressions
-  var hasUpperCase = /[A-Z]/.test(password);
-  var hasLowerCase = /[a-z]/.test(password);
-  var hasNumbers = /\d/.test(password);
-  var hasMinLength = password.length >= 8;
-   
-  // Display message to user if password does not meet criteria
-  if (!hasUpperCase || !hasLowerCase || !hasNumbers || !hasMinLength) {
-     document.getElementById('message').textContent = 'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one number.';
-  } else {
-     document.getElementById('message').textContent = '';
-     // Add code here to submit the form or process the password as needed
-  }
- });
-
+import { AppModule } from './app/app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
