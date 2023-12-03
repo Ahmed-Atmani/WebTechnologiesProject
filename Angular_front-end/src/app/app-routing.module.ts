918a0776b31aeaa84b5f4106be21e5849e29aee3
Ahmed Atmani <ahmed.khalil-allah.atmani@vub.be>
2023-12-03 21:55:10 +0100

    Fixed errors

--- Angular_front-end/src/app/app-routing.module.ts
+++ Angular_front-end/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { ShowAccountComponent } from './account/show-account/show-account.component';
 import { AccountOverviewComponent } from './account/account-overview/account-overview.component';
+import { RegisterComponent } from './account/register/register.component';
 
 
 const routes: Routes = [
   {path:'account-test', component:ShowAccountComponent},
   {path:'home', component:AccountComponent},
   {path:'account-overview', component:AccountOverviewComponent},
+  {path:'register', component:RegisterComponent},
 ];
 
 @NgModule({
