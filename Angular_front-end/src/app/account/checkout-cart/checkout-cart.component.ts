79486f6cfd259e4c3d36ee1f7f96cf3512ca22fe
marouane.benslimane <marouane.benslimane@vub.be>
2023-12-12 12:33:07 +0100

    new modules added to show the items (visualisation not done yet)

---
+++ Angular_front-end/src/app/account/checkout-cart/checkout-cart.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-cart',
+  templateUrl: './checkout-cart.component.html',
+  styleUrls: ['./checkout-cart.component.css']
+})
+export class CheckoutCartComponent {
+
+  ItemList: any = [];
+  TotalPrice: number = 0;
+
+  ngOnInit(): void {
+    // Get the shopping cart
+    this.ItemList = JSON.parse(localStorage.getItem("ItemList") || "[]");
+
+    this.ItemList.forEach((item: any) => {
+        this.TotalPrice += parseFloat(item["ItemPrice"]) * parseInt(item["PurchaseAmount"]);
+    });
+    this.TotalPrice = Math.round(this.TotalPrice * 100) / 100;
+  }
+
+}
