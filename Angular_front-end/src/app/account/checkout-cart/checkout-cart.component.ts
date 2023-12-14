258128a11670c0caa9257ccdf951fd2788decef1
Ahmed Atmani <ahmed.khalil-allah.atmani@vub.be>
2023-12-14 20:43:45 +0100

    Added working map (not yet adapted to requirements)

--- Angular_front-end/src/app/account/checkout-cart/checkout-cart.component.ts
+++ Angular_front-end/src/app/account/checkout-cart/checkout-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-checkout-cart',
@@ -11,13 +13,34 @@ export class CheckoutCartComponent {
   TotalPrice: number = 0;
 
   ngOnInit(): void {
-    // Get the shopping cart
-    this.ItemList = JSON.parse(localStorage.getItem("ItemList") || "[]");
-
-    this.ItemList.forEach((item: any) => {
-        this.TotalPrice += parseFloat(item["ItemPrice"]) * parseInt(item["PurchaseAmount"]);
-    });
-    this.TotalPrice = Math.round(this.TotalPrice * 100) / 100;
+    this.getCartItems();
+    this.initLeafletMap();
   }
 
+  initLeafletMap(): void {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    const latitude = 49.327380;
+    const longitude = 23.754300;
+    const altitude = 0;
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // L.marker([51.505, -0.09]).addTo(map)
+    //   .bindPopup('A sample marker.');
+  }
+
+  getCartItems(): void {
+    this.ItemList = JSON.parse(localStorage.getItem("ItemList") || "[]");
+    this.TotalPrice = this.calcTotalPrice();
+  }
+
+  calcTotalPrice(): number {
+    this.ItemList.forEach((item: any) => {
+      this.TotalPrice += parseFloat(item["ItemPrice"]) * parseInt(item["PurchaseAmount"]);
+    });
+    return Math.round(this.TotalPrice * 100) / 100;
+  }
 }
