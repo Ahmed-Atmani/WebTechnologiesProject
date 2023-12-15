5688dde5382f84d265ef8d657cadb3ab22546052
Ahmed Atmani <ahmed.khalil-allah.atmani@vub.be>
2023-12-15 14:29:52 +0100

    Added geocoding function

--- Angular_front-end/src/app/account/checkout-cart/checkout-cart.component.ts
+++ Angular_front-end/src/app/account/checkout-cart/checkout-cart.component.ts
@@ -12,25 +12,88 @@ export class CheckoutCartComponent {
   ItemList: any = [];
   TotalPrice: number = 0;
   ShopLocation: any = { "latitude": 50.844278, "longitude": 2.961053 };
+  map: any = [];
 
   ngOnInit(): void {
     this.getCartItems();
     this.initLeafletMap();
+    this.initGeoCoder();
   }
 
   initLeafletMap(): void {
-    const map = L.map('map').setView([51.505, -0.09], 13);
-
-    const latitude = 49.327380;
-    const longitude = 23.754300;
-    const altitude = 0;
-
+    this.map = L.map('map').setView([this.ShopLocation.latitude, this.ShopLocation.longitude], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+    }).addTo(this.map);
+  }
 
-    L.marker([51.505, -0.09]).addTo(map)
-      .bindPopup('A sample marker.');
+  addMarkerToMap(longitude: number, latitude: number, name: string): void {
+    L.marker([longitude, latitude]).addTo(this.map).bindPopup(name);
+  }
+
+  initGeoCoder(): void {
+    var api_key = 'cc863016aa0e4d35a9725dd2c9b8cb70';
+
+    // reverse geocoding example (coordinates to address)
+    // var latitude = '52.3877830';
+    // var longitude = '9.7334394';
+    // var query = latitude + ',' + longitude;
+
+    // forward geocoding example (address to coordinate)
+    var query = 'Vrije Universiteit Brussel, Belgium';
+    // var query = 'Philipsbornstr. 2, 30165 Hannover, Germany';
+    // note: query needs to be URI encoded (see below)
+
+    var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+    var request_url = api_url
+      + '?'
+      + 'key=' + api_key
+      + '&q=' + encodeURIComponent(query)
+      + '&pretty=1'
+      + '&no_annotations=1';
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    var request = new XMLHttpRequest();
+    request.open('GET', request_url, true);
+
+    request.onload = () => {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status === 200){
+        // Success!
+        var data = JSON.parse(request.responseText);
+        // alert(JSON.stringify(data, null, 4));
+        // alert(data.results[0].formatted); // print the location
+        // alert("lat: " + data.results[0].bounds.northeast.lat);
+        // alert("lng: " + data.results[0].bounds.northeast.lng);
+
+        var lat: number = data.results[0].bounds.northeast.lat;
+        var long: number = data.results[0].bounds.northeast.lng;
+
+        this.addMarkerToMap(lat, long, query);
+
+
+      } else if (request.status <= 500){
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log('error msg: ' + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send();  // make the request
   }
 
   getCartItems(): void {
