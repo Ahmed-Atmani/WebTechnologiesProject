7b1c7f1258315d2eb1f857881359940819a76451
Souhaila <souhaila.elmokhtari@outlook.com>
2023-12-17 18:06:04 +0100

    Installation angular/material + some changes in account-orders-component

---
+++ Angular_front-end/src/app/account/account-orders/package-tracking/package-tracking.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-tracking',
+  templateUrl: './package-tracking.component.html',
+  styleUrls: ['./package-tracking.component.css']
+})
+export class PackageTrackingComponent {
+
+}
