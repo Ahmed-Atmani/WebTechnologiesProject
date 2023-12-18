155b28a7d629330287be57b7663e7b278f240e8d
Souhaila <souhaila.elmokhtari@outlook.com>
2023-12-18 21:43:09 +0100

    Login and login button work

--- Angular_front-end/src/app/account/account-overview/account-overview.component.ts
+++ Angular_front-end/src/app/account/account-overview/account-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/login.service';
 import { SharedService } from 'src/app/shared.service';
 
@@ -7,28 +7,58 @@ import { SharedService } from 'src/app/shared.service';
   templateUrl: './account-overview.component.html',
   styleUrls: ['./account-overview.component.css']
 })
-
-
-
-export class AccountOverviewComponent {
+export class AccountOverviewComponent implements OnInit {
   currentAccount: any = null;
+  editedAccount: any = null;
+  updatedAccount: any = {};
 
   constructor(public login: LoginService, public service: SharedService) {
     this.fillAccount();
   }
 
+  ngOnInit(): void {
+    this.updatedAccount = { ...this.editedAccount };
+  }
+
   fillAccount() {
     const accountIdString = this.login.getAccountId();
     const accountIdNumber = parseInt(accountIdString, 10);
-    
+
     this.service.getLoggedInAccount(accountIdNumber).subscribe(
       data => {
         this.currentAccount = data;
+        this.editedAccount = { ...data };
         console.log(data);
-        
       },
       error => {
         console.error('Error fetching account data:', error);
+      }
+    );
+    
+  }
+  deleteAccount(accountId: string) {
+    if (confirm('Are you sure you want to delete your account?')) {
+      this.service.deleteAccount(accountId).subscribe(
+        data => {
+          console.log('Account deleted successfully:', data);
+        },
+        error => {
+          console.error('Error deleting account:', error);
+        }
+      );
+    }
+  }
+
+  saveAccount() {
+    this.updatedAccount = { ...this.editedAccount };
+
+    this.service.updateAccount(this.currentAccount.AccountId, this.updatedAccount).subscribe(
+      data => {
+        console.log('Account updated successfully:', data);
+        this.currentAccount = { ...this.updatedAccount };
+      },
+      error => {
+        console.error('Error updating account:', error);
       }
     );
   }
