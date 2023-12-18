98838267468ab8def527ae8b441b79c4e2ee5495
marouane.benslimane <marouane.benslimane@vub.be>
2023-12-18 15:37:06 +0100

    mods in omnipoints page

--- Angular_front-end/src/app/account/account-points/account-points.component.ts
+++ Angular_front-end/src/app/account/account-points/account-points.component.ts
@@ -10,15 +10,20 @@ import { SharedService } from 'src/app/shared.service';
 export class AccountPointsComponent {
   currentAccount: any = null;
   lastClickedPoints: number | null = null;
+  confirmationData = {
+    showConfirmation: false,
+    points: 0,
+    redemptionMessage: ''
+  };
 
   constructor(public login: LoginService, public service: SharedService) {
     this.fillAccount();
   }
 
-  fillAccount() {
+  fillAccount(): void {
     const accountIdString = this.login.getAccountId();
     const accountIdNumber = parseInt(accountIdString, 10);
-    
+
     this.service.getLoggedInAccount(accountIdNumber).subscribe(
       data => {
         this.currentAccount = data;
@@ -30,11 +35,37 @@ export class AccountPointsComponent {
     );
   }
 
-  redeemPoints(points: number): void {
-    this.lastClickedPoints = points;
+  showConfirmation(points: number): void {
+    this.confirmationData.showConfirmation = true;
+    this.confirmationData.points = points;
+    this.confirmationData.redemptionMessage = '';
+  }
+
+  confirmRedeem(): void {
+    if (this.checkAccountPoints(this.confirmationData.points)) {
+      this.confirmationData.showConfirmation = false;
+      this.confirmationData.points = 0;
+      this.confirmationData.redemptionMessage = 'Points redeemed successfully!';
+    } else {     
+      this.confirmationData.redemptionMessage = 'Not enough Omnipoints for redemption.';
+    }
   }
 
   checkAccountPoints(points: number): boolean {
     return this.currentAccount?.AccountPoints > points;
+  }
+
+  calculateVoucherAmount(points: number): string {
+    if (points === 100) {
+      return '€10 voucher';
+    } else if (points === 200) {
+      return '€25 voucher';
+    } else if (points === 300) {
+      return '€50 voucher';
+    } else if (points === 400) {
+      return '€100 voucher';
+    } else {
+      return 'Unknown voucher amount';
+    }
   }
 }
