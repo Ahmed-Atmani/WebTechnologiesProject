9d1f7b4f16caae8e20f5e4c7cd72bfc63e5eabed
marouane.benslimane <marouane.benslimane@vub.be>
2023-12-18 12:55:06 +0100

    test commit for souhaila

--- Angular_front-end/src/app/account/account-points/account-points.component.ts
+++ Angular_front-end/src/app/account/account-points/account-points.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/login.service';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-account-points',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-points.component.css']
 })
 export class AccountPointsComponent {
+  currentAccount: any = null;
+  
+  constructor(public login: LoginService, public service: SharedService) {
+    this.fillAccount();
+  }
 
+  fillAccount() {
+    const accountIdString = this.login.getAccountId();
+    const accountIdNumber = parseInt(accountIdString, 10);
+    
+    this.service.getLoggedInAccount(accountIdNumber).subscribe(
+      data => {
+        this.currentAccount = data;
+        console.log(data);
+        
+      },
+      error => {
+        console.error('Error fetching account data:', error);
+      }
+    );
+  }
 }
