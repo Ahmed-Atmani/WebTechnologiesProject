9590f42ccaf62752ae6cb7bd87c569a280636848
Ahmed Atmani <ahmed.khalil-allah.atmani@vub.be>
2023-12-20 12:10:15 +0100

    none

--- Angular_front-end/src/app/order-finished/order-finished.component.ts
+++ Angular_front-end/src/app/order-finished/order-finished.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class OrderFinishedComponent implements OnInit{
 
   ngOnInit(): void {
-    // this.ClearCart();
+    this.ClearCart();
   }
 
   ClearCart(): void {
